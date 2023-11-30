936d369946108cc02a888b8a9745b470b4c3b6ac
Jiongran Wang <jw770@tamu.edu>
2023-11-29 23:00:49 -0600

    modify NAMESPACE

---
+++ LTNMD/R/Imports.R
@@ -0,0 +1,10 @@
+#' @importFrom mvtnorm rmvnorm
+#' @importFrom mvtnorm dmvnorm
+#' @importFrom hesim rcat
+#' @importFrom stats rbeta
+#' @importFrom stats qlogis
+#' @importFrom stats rgamma
+#' @importFrom stats rbinom
+#' @importFrom gtools rdirichlet
+#' @importFrom BayesLogit rpg.sp
+NULL
